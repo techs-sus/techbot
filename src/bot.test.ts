531f07b837f73bce3e40e48dba78c96dce3b2a4d
techs-sus <92276908+techs-sus@users.noreply.github.com>
2022-01-31 21:22:58 +0000

    test

--- src/bot.test.ts
+++ src/bot.test.ts
@@ -2,22 +2,24 @@ import bot from "./bot";
 
 it("should be able to connect to trollbox.party", () => {
 	return new Promise((resolve) => {
-		let instance = new bot("techbot [jest]", "#fff", () => {});
-		let botsaidstuff = false;
-		instance.onMessage = (msg) => {
-			if (msg.msg == "helo! " + new Date().getDay()) {
-				botsaidstuff = true;
-			}
-		};
-		instance.init();
-		instance.sendMessage("helo! " + new Date().getDay());
-		let s = setInterval(() => {
-			if (botsaidstuff === true) {
-				clearInterval(s);
-				expect(botsaidstuff).toBe(true);
-				instance.disconnect();
-				resolve(true);
-			}
-		}, 50);
+		let instance = new bot("techbot [jest]", "#fff", () => {
+			let botsaidstuff = false;
+			instance.onMessage = (msg) => {
+				if (msg.msg == "helo! " + new Date().getDay()) {
+					botsaidstuff = true;
+				}
+			};
+			instance.init();
+			instance.sendMessage("helo! " + new Date().getDay());
+			// setTimeout(() => (botsaidstuff = true), 1000); // only for use on gitpod
+			let s = setInterval(() => {
+				if (botsaidstuff === true) {
+					clearInterval(s);
+					expect(botsaidstuff).toBe(true);
+					instance.disconnect();
+					resolve(true);
+				}
+			}, 50);
+		});
 	});
-}, 50000);
+}, 5000);
