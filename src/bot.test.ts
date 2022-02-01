853f974054265049b88124373c6f77572098d864
techs-sus <92276908+techs-sus@users.noreply.github.com>
2022-02-01 16:38:49 +0000

    aaa

--- src/bot.test.ts
+++ src/bot.test.ts
@@ -3,14 +3,15 @@ import bot from "./bot";
 it("should be able to connect to trollbox.party", () => {
   return new Promise((resolve) => {
     let instance = new bot("techbot [jest]", "#fff", () => {
+      let detection = "helo!!! this is node ci, its " + new Date().toDateString()
       let botsaidstuff = false;
       instance.onMessage = (msg) => {
-        if (msg.msg == "helo! " + new Date().getDay()) {
+        if (msg.msg == detection) {
           botsaidstuff = true;
         }
       };
       instance.init();
-      instance.sendMessage("helo! " + new Date().getDay());
+      instance.sendMessage(detection);
       // setTimeout(() => (botsaidstuff = true), 1000); // only for use on gitpod
       let s = setInterval(() => {
         if (botsaidstuff === true) {
